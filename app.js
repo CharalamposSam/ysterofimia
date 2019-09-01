e0b530a48f9d3e443b0c08ecc57e9bc80a1a8c6f
Charalampos <chara.lampos.samou.ridis@gmail.com>
2019-09-01 15:46:03 +0300

    adfb

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ menuBtn.addEventListener( 'click', () => {
         flag = true
     }
 } )
+window.addEventListener( 'resize', () => {
+    if ( window.innerWidth > 700 ) {
+        basicMenu.style.height = ` auto `
+        menuArrow.style.transform = ` rotateX( 0 ) `
+    } else if ( window.innerWidth < 700 && basicMenu.style.height == `auto` ) {
+        basicMenu.style.height = ` 1px `
+        flag = true
+    }
+} )
+
